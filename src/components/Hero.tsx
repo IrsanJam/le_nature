19c017d6f48d15dfd5946c466bf33efc49e2d73d
IrsanJam <irsandjamaludin@gmail.com>
2024-04-24 07:33:06 +0700

    add button shop

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,8 @@
+import { useNavigate } from "react-router-dom";
 import hero from "/image/Image 1.png";
 
 const Hero = () => {
+  const navigate = useNavigate();
   return (
     <div className="w-full relative">
       <div className="w-full h-[70vh]">
@@ -9,7 +11,9 @@ const Hero = () => {
       <div className="absolute md:top-[35%] top-[35%] md:w-[60%] w-[80%] flex flex-col pl-10 md:pl-20 gap-4">
         <h1 className="font-bold md:text-6xl  text-5xl font-Poppins text-zinc-700">Gift for your skin</h1>
         <span className="md:w-[60%] w-full md:text-base text-sm tracking-wide font-thin text-neutral-500">Discover your skin's ultimate glow with our luxurious gift embrace unmatched hydration and transformative</span>
-        <button className="text-white bg-[#00BDD6] w-[50%] md:w-[20%] p-3 rounded-md hover:opacity-60 font-semibold font-Poppins">Shop Now</button>
+        <button onClick={() => navigate("/shop")} className="text-white bg-[#00BDD6] w-[50%] md:w-[20%] p-3 rounded-md hover:opacity-60 font-semibold font-Poppins">
+          Shop Now
+        </button>
       </div>
     </div>
   );
